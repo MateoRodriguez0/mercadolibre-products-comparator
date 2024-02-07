4938e6b6b44e592c2b2e0503169a7beaf249c071
mateo <mateo204r@gmail.com>
2024-02-06 19:02:57 -0500

    implementando spring security en eureka server

--- eureka-server/src/main/java/com/mercadolibre/productscomparator/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/mercadolibre/productscomparator/eurekaserver/EurekaServerApplication.java
@@ -3,6 +3,10 @@ package com.mercadolibre.productscomparator.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,5 +16,12 @@ public class EurekaServerApplication {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
-
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authz) -> authz
+	        .anyRequest().authenticated())
+		    .httpBasic(Customizer.withDefaults());
+	    http.csrf(t -> t.ignoringRequestMatchers("/eureka/**"));
+	    return http.build();
+	}
 }
